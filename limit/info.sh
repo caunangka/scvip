6c4c6bb9369ff9853369db76b5270824e0dc4b36
YogzSC <hernizeh@gmail.com>
2024-06-02 02:23:45 +0700

    Add files via upload

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "YOGZ PROJECT TUNNELING" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @YSSHstore" | lolcat
+echo "                   Author Script YOGZ PROJECT" | lolcat
+echo "-----------------------------------------------------"
